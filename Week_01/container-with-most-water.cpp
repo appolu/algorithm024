04be1f255b8ff24e58b66d5f383f24c6f3b61222
phylony <phylony@gmail.com>
2021-01-25 23:52:42 +0800

    Create container-with-most-water.cpp
    Creation:container with most water

---
+++ Week_01/container-with-most-water.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+class solution
+{
+public:
+	int maxArea(vector<int > heights) {
+		int max = 0;
+		for (int i = 0, j = heights.size() - 1; i < j;) {
+			int w = j - i;
+			int h = heights[j] > heights[i] ? heights[i++] : heights[j--];//判断首尾高度，低得一方，往中间缩进。
+			int area = w * h;
+			if (area > max) {
+				max = area;
+			}
+		}
+		return max;
+	}
+
+};
