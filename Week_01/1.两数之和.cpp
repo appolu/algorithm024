b2c35475042ba132a95096c2868b1ff3163f0bb5
abolo <abolo@vip.qq.com>
2021-01-30 20:52:34 +0800

    Create 1.两数之和.cpp

---
+++ Week_01/1.两数之和.cpp
@@ -0,0 +1,31 @@
+//
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		map<int, int> values;
+		vector<int> elements;
+
+		for (int i = 0; i < nums.size(); i++) {
+			int key = nums[i];
+			int num = find(target - key, values);
+
+			if (num >= 0) {
+				elements.push_back(num);
+				elements.push_back(i);
+				return elements;
+
+			}
+			values.insert(pair<int, int>(key, i));
+		}
+		return elements;
+
+	}
+
+	int find(int x, map<int, int> &values) {
+		map<int, int>::iterator iter = values.find(x);
+		if (iter != values.end()) {
+			return iter->second;
+		}
+		return -1;
+	}
+};
