7b32ce7b29c2de013c708d222b7c03b7e9d471b6
abolo <abolo@vip.qq.com>
2021-02-05 00:15:59 +0800

    二叉树

---
+++ Week_02/144.二叉树得前序遍历.cpp
@@ -0,0 +1,14 @@
+//https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
+class Solution {
+public:
+    vector<int> result;
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(root==nullptr){
+            return result;
+        }
+        result.push_back(root->val);
+        preorderTraversal(root->left);
+        preorderTraversal(root->right);
+        return result;
+    }
+};
